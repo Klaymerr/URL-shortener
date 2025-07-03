87e06fa7775999df9d92bff04f6b3504811d546c
Иван Демидов <hekotn@mail.ru>
2025-07-03 22:28:32 +0300

    Update main.go

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,63 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io"
+	"math/rand/v2"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+)
+
+var shortToOriginal = make(map[string]string)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		if r.Header.Get("Content-Type") != "text/plain" {
+			http.Error(w, "Content-Type not supported", http.StatusUnsupportedMediaType)
+			return
+		}
+
+		text, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Failed to read body", http.StatusInternalServerError)
+			return
+		}
+
+		_, err = url.ParseRequestURI(string(text))
+		if err != nil {
+			http.Error(w, "Invalid URL", http.StatusInternalServerError)
+			return
+		}
+		fmt.Println(string(text))
+		newURL := strconv.FormatInt(rand.Int64(), 36)
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte("http://localhost:8080/" + newURL))
+		shortToOriginal[newURL] = string(text)
+		return
+	}
+
+	if r.Method == http.MethodGet {
+		id := strings.TrimPrefix(r.URL.Path, "/")
+		orig, ok := shortToOriginal[id]
+		if !ok {
+			http.Error(w, "Not Found", http.StatusNotFound)
+			return
+		}
+		http.Redirect(w, r, orig, http.StatusMovedPermanently)
+		return
+	}
+
+	http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/`, handler)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
