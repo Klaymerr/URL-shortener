92f10933dfb58c359ff7d1b7dfc010ba5e15414f
Иван Демидов <hekotn@mail.ru>
2025-07-05 13:37:33 +0300

    use gin framework

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,66 +1,56 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
 	"io"
 	"math/rand/v2"
-	"mime"
 	"net/http"
 	"net/url"
 	"strconv"
-	"strings"
 )
 
 var shortToOriginal = make(map[string]string)
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		fmt.Println(r.Header.Get("Content-Type"))
-		mediaType, _, err := mime.ParseMediaType(r.Header.Get("Content-Type"))
-		if err != nil || mediaType != "text/plain" {
-			http.Error(w, "Content-Type not supported", http.StatusUnsupportedMediaType)
-			return
-		}
+func getting(c *gin.Context) {
+	id := c.Param("id")
+	orig, ok := shortToOriginal[id]
+	if !ok {
+		c.String(http.StatusNotFound, "Not Found")
+		return
+	}
+	c.Redirect(http.StatusTemporaryRedirect, orig)
+}
 
-		text, err := io.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "Failed to read body", http.StatusInternalServerError)
-			return
-		}
-
-		_, err = url.ParseRequestURI(string(text))
-		if err != nil {
-			http.Error(w, "Invalid URL", http.StatusInternalServerError)
-			return
-		}
-		fmt.Println(string(text))
-		newURL := strconv.FormatInt(rand.Int64(), 36)
-		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte("http://localhost:8080/" + newURL))
-		shortToOriginal[newURL] = string(text)
+func posting(c *gin.Context) {
+	mediaType := c.ContentType()
+	if mediaType != "text/plain" {
+		c.String(http.StatusUnsupportedMediaType, "Content-Type not supported")
 		return
 	}
 
-	if r.Method == http.MethodGet {
-		id := strings.TrimPrefix(r.URL.Path, "/")
-		orig, ok := shortToOriginal[id]
-		if !ok {
-			http.Error(w, "Not Found", http.StatusNotFound)
-			return
-		}
-		http.Redirect(w, r, orig, http.StatusTemporaryRedirect)
+	body, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Failed to read body")
 		return
 	}
 
-	http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	rawURL := string(body)
+	_, err = url.ParseRequestURI(rawURL)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Invalid URL")
+		return
+	}
+
+	newID := strconv.FormatInt(rand.Int64(), 36)
+	shortToOriginal[newID] = rawURL
+
+	c.String(http.StatusCreated, "http://localhost:8080/"+newID)
 }
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc(`/`, handler)
+	router := gin.Default()
+	router.GET("/:id", getting)
+	router.POST("/", posting)
 
-	err := http.ListenAndServe(`:8080`, mux)
-	if err != nil {
-		panic(err)
-	}
+	router.Run("localhost:8080")
 }
